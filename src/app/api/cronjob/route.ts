2e59d98b408f27dbe9d947620df87c2d94dfadb4
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2026-03-27 01:27:29 +0530

    Made an api endpoint for cronjob so that my supabase never gets paused.

---
+++ src/app/api/cronjob/route.ts
@@ -0,0 +1,11 @@
+import { supabase } from "../../lib/supabase"
+
+export async function GET(req: Request) {
+    const { data, error } = await supabase
+        .from("cronjob")
+        .select("*")
+    if (error) {
+        return Response.json({ error: error.message }, { status: 500 })
+    }
+    return Response.json(data);
+}
